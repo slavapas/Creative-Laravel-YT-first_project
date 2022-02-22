1c2a19fcadac0761ebd0fe1713875eb1e8db26f7
Slava Pascaru <slavapascaru@gmail.com>
2022-02-22 22:23:38 +0200

    First commit

---
+++ app/Http/Controllers/MyActionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyActionController extends Controller
+{
+    public function go()
+    {
+        return "Hi MyActionController";
+    }
+}
---
+++ app/Http/Controllers/MyAtomicController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyAtomicController extends Controller
+{
+    public function run()
+    {
+        return "Hi MyAtommicControoller";
+    }
+}
